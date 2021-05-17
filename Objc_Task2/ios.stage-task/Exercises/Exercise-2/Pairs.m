63a594edcf333f3cd2322ff7eddcddd5b76074ff
Signtone <actionx@list.ru>
2021-05-17 23:42:13 +0300

    feat: add solutions

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,19 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger count = 0;
+	if (array.count > 1) {
+		for (int i = 0; i < array.count - 1; i++) {
+			for (int n = i + 1; n < array.count; n++) {
+				long diff = [array[i] longValue] - [array[n] longValue];
+				diff *= (diff < 0) ? -1 : 1;
+				if (diff == [number longValue]) {
+					count++;
+				}
+			}
+		}
+	}
+    return count;
 }
 
 @end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,6 +3,39 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
+    NSMutableArray * numArray = [NSMutableArray new];
+    NSMutableArray * strArray = [NSMutableArray new];
+    NSMutableArray * mixedArray = [NSMutableArray new];
+    
+    if ([array[0] isKindOfClass:[NSString class]] || [array[0] isKindOfClass:[NSNumber class]]) {
+        return @[];
+    } else {
+        for (int i = 0; i < array.count; i++) {
+            for (int j = 0; j < array[i].count; j++) {
+                if ([array[i][j] isKindOfClass:[NSNumber class]]) {
+                    [numArray addObject:array[i][j]];
+                } else {
+                    [strArray addObject:array[i][j]];
+                }
+                
+            }
+        }
+    }
+    
+    [numArray sortUsingSelector: @selector(compare:)];
+    [strArray sortUsingSelector: @selector(compare:)];
+    
+    if ((strArray.count != 0) && (numArray.count != 0)) {
+        NSArray* reversedArray = [[strArray reverseObjectEnumerator] allObjects];
+        [mixedArray addObject: numArray];
+        [mixedArray addObject: reversedArray];
+        return mixedArray;
+    } else if (numArray.count != 0) {
+        return numArray;
+    } else if (strArray.count != 0) {
+        return   strArray;
+    }
+    
     return @[];
 }
 
